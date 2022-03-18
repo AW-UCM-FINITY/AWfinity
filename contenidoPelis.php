d06c36b86d8460ea142d3ac5f93e3825eed02ed3
sansan12 <sansan12@ucm.es>
2022-03-18 18:35:29 +0100

    cambio id contenidoPelis.php

--- contenidoPelis.php
+++ contenidoPelis.php
@@ -7,10 +7,10 @@ $tituloPagina = 'Películas';
 $contenidoPrincipal = "";
 
 
-$contenidoPrincipal .=  "<div class='contenidoPelis' id='contenidoPelis'>";
+$contenidoPrincipal .=  "<div class='contenidoPelis'>";
 
 $contenidoPrincipal .= 
-"<div class='tituloIndex' id ='tituloIndexPeliculas'>
+"<div class='tituloIndex'>
     <h2>AWfinity </h2>
 	<h1><span>Películas</span> todos los géneros y más </h1>";
     
@@ -25,9 +25,7 @@ if(isset( $_SESSION['esEditor']) &&  $_SESSION['login']==true && $_SESSION['esEd
 	</div>
 	</form>
 
-EOS;* */
-
-	
+EOS;* */	
 }
 $contenidoPrincipal .= "</div>";
 //$formC = new path\FormEditorEditPeli();
@@ -37,8 +35,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO ACCION */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE ACCIÓN</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE ACCIÓN</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("accion");
 foreach ($arrayPelis as $key => $value) {
  	$ruta = $value->getRutaImagen();
@@ -51,8 +49,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO ANIME */
 $contenidoPrincipal .="<div class='pelisIndex'>"; 
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE ANIME</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE ANIME</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("anime");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
@@ -65,7 +63,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO CIENCIA FICCION */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE CIENCIA FICCIÓN</h3> </div>";$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE CIENCIA FICCIÓN</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("ciencia ficcion");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
@@ -78,8 +77,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO COMEDIA */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE COMEDIA</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE COMEDIA</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("comedia");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
@@ -93,8 +92,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO DRAMA */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE DRAMA</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE DRAMA</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("drama");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
@@ -107,8 +106,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO FANTASIA */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE FANTASÍA</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE FANTASÍA</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("fantasia");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
@@ -122,8 +121,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO MUSICAL */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE MUSICAL</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE MUSICAL</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("musical");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
@@ -136,8 +135,8 @@ $contenidoPrincipal .= "</div>";
 
 /**MOSTRAMOS PELICULAS GENERO TERROR */
 $contenidoPrincipal .="<div class='pelisIndex'>";
-$contenidoPrincipal.= "<div class='tituloPeliIndex' id='lista-peli'> <h3>PELÍCULAS DE TERROR</h3> </div>";
-$contenidoPrincipal .= "<div class='peliLista' id='pelis'>";
+$contenidoPrincipal.= "<div class='tituloPeliIndex'> <h3>PELÍCULAS DE TERROR</h3> </div>";
+$contenidoPrincipal .= "<div class='peliLista'>";
 $arrayPelis = path\Pelicula::ordenarPor("TERROR");
 foreach ($arrayPelis as $key => $value) {
 	$id_pelicula = $value->getId();
