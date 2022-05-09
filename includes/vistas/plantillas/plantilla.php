2aa228b34710430cb75502bc7dc73f6ee0df5608
sansan12 <sansan12@ucm.es>
2022-05-09 18:51:07 +0200

    cambiarcss y temporada vista fuera

--- includes/vistas/plantillas/plantilla.php
+++ includes/vistas/plantillas/plantilla.php
@@ -4,7 +4,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" id="estilo" type="text/css" href="<?= RUTA_CSS?>/<?= es\ucm\fdi\aw\Apariencia::getAspecto()->getCss();?>"/>
-    <script type="text/javascript" src="<?= RUTA_JS ?>/cambiarcss.js"></script>
     
     <link rel="stylesheet" type="text/css" href="<?= RUTA_CSS ?>/fonts.css"/>
     
